eb7404bd55e837a77632a82ca8ad595955b24347
MEBANNED <frankchencty@gmail.com>
2026-01-08 08:55:57 +0000

    first-lesson

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -1,4 +1,4 @@
-print("Hello from lesson 1")
+# print("Hello from lesson 1")
 
 ######### The below is the code required for task 2a ##########
 # print("1")
@@ -59,59 +59,137 @@ print("Hello from lesson 1")
 # print("2030")
 
 ############## Below is the code for task 2b ##############
-print("1")
-print("2")
-print("3")
-print("4")
-print("5")
-print("6")
-print("7")
-print("8")
-print("9")
-print("10")
-print("11")
-print("12")
-print("13")
-print("14")
-print("15")
-print("16")
-print("17")
-print("18")
-print("19")
-print("20")
-print("21")
-print("22")
-print("23")
-print("24")
-print("25")
-print("26")
-print("27")
-print("28")
-print("29")
-print("30")
-print("31")
+# print("1")
+# print("2")
+# print("3")
+# print("4")
+# print("5")
+# print("6")
+# print("7")
+# print("8")
+# print("9")
+# print("10")
+# print("11")
+# print("12")
+# print("13")
+# print("14")
+# print("15")
+# print("16")
+# print("17")
+# print("18")
+# print("19")
+# print("20")
+# print("21")
+# print("22")
+# print("23")
+# print("24")
+# print("25")
+# print("26")
+# print("27")
+# print("28")
+# print("29")
+# print("30")
+# print("31")
 
-print("January")
-print("February")
-print("March")
-print("April")
-print("May")
-print("June")
-print("July")
-print("August")
-print("September")
-print("October")
-print("November")
-print("December")
+# print("January")
+# print("February")
+# print("March")
+# print("April")
+# print("May")
+# print("June")
+# print("July")
+# print("August")
+# print("September")
+# print("October")
+# print("November")
+# print("December")
 
-print("2020")
-print("2021")
-print("2022")
-print("2023")
-print("2024")
-print("2025")
-print("2026")
-print("2027")
-print("2028")
-print("2029")
-print("2030")
+# print("2020")
+# print("2021")
+# print("2022")
+# print("2023")
+# print("2024")
+# print("2025")
+# print("2026")
+# print("2027")
+# print("2028")
+# print("2029")
+# print("2030")
+
+# Task 4a
+# print("Hello World")
+
+# Task 4b
+# print(2015)
+# print("2015")
+
+## Task 5: print() is how you say stuff
+# Task: Learn about the print() function.
+# Print the following:
+# 1. Hello world
+# 2. My favourite food is <insert your favourite>
+# 3. My birthday is on <insert your birthday >
+print("My Favourite Food is Dumplings")
+print("my birthday is on DEC16")
+
+
+## Task 6: Self-Introduction
+# Task: Introduce yourself. Write a program to introduce yourself
+# using print statements
+
+# You should print out
+# 1. Your name
+# 2. How old you are
+# 3. What is your CCA?
+# 4. If you had $2,000 as a reward, what would you buy?
+# print("Frank")
+# print("10")
+# print("I don't have one(yet)")
+# print("save it")
+
+## Task 7: Even more printing!
+# Using print statements, write out your morning routine until
+# you get to school. An example is given below.
+
+# 1. Wake up!
+# 2. Go toilet... (remember to flush)
+# 3. Brush your teeth...
+print("1. wake up")
+print("change clothes")
+print("brush teeth")
+print("eat breakfast")
+print("go to to school")
+
+## Task 8: Breakfast Routine
+# Rearrange the following to reflect a typical sequence for
+# making breakfast:
+
+# print("Pour milk and cereal into a bowl.")
+# print("Wake up and get out of bed.")
+# print("Eat your breakfast.")
+
+# print("Wake up and get out of bed.")
+# print("Pour milk and cereal into a bowl.")
+# print("Eat your breakfast.")
+
+
+## Task 9: Find the errors
+# Task: Find the errors. 
+# Copy (ctrl + c) and paste (ctrl +v) the below codes into
+# lesson1.py. They are wrong. Fix them.
+
+# print(Hello and how are you?)
+
+# print("What is wrong with this"
+
+# print("Why is this not working?)
+
+# Prnt("Why is this not working?")
+
+print("Hello and how are you?")
+
+print("What is wrong with this")
+
+print("Why is this not working?")
+
+print("Why is this not working?")
--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,3 +5,95 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+## Recap 1: Colours of the Rainbow
+# Task: 
+# 1. Using a new line for each colour, print out the 7
+#    colours of the rainbow.
+# 2. Then, comment out every other line except for your
+#    favourite colour.
+
+# --------------------------------------------------------
+
+# ## Task 1: Solve the problem in pseudocode
+# **Task 1a**:
+# Task: Look at the screen and translate the code blocks
+# into pseudocode
+#  repeat the following 10 times:
+#     say "Hey!"
+#     walk ten steps
+#
+
+
+# **Task 1b**:
+# Task: Look at the screen and translate the code blocks
+# into pseudocode
+# set a variable called "counter" to 5
+# repeat the following until "counter" is equivelent to 50:
+#     walk ten steps forward
+#     turn fifteen defrees
+#     move "counter" up by 1
+# **Task 1c**:
+# Task: Look at the screen and translate the code blocks
+# into pseudocode
+
+# ask the user for his/her age
+# if the user is under 18 then
+#     say "access denied"
+# else
+#     say "welcome"
+
+# ## Task 2: Recycling Logic
+# Task: Write down the steps on how to solve the problem below
+
+# Design pseudocode for a recycling robot that sorts items
+# into bins for glass, plastic, and paper. The robot should
+# check each item's material and place it in the correct bin.
+# repeat until there is no more stuff
+#     make the robot check material
+#     if the meterial is plastic
+#         throw into plastic bin
+#     elif the material is paper
+#         throw the item into the paper bin
+#     elif the material is glass
+#         throw the object into glass bin
+#     else 
+#        throw into the general waste bin
+
+# ## Task 3: What's the password?
+# Task: Using pseudocode, design a program that will check if the
+# user has entered the right secret phrase.
+
+# 1. If the user has entered a wrong secret phrase, send them an
+# error message.
+# 1. If the user has entered the right secret phrase, congratulate
+# them.
+
+# set password as yellow2222026!
+# say ask the user "ENTER PASSWORd" and store into "userans"
+# check if the user has entered the correct phase
+#     if the user has not
+#         print out "INCORRECT AREA, PLEASE LEAVE"
+#     if the user has
+#         print out "congratulations"
+
+
+## Task 4: What's your grade?
+# Task: Write a pseudocode that calculates the final score of a student
+# based on the following weightage:
+# _each test is scored upon 100_
+# Test 1 = 20%
+# Test 2 = 40%
+# Test 3 = 40%
+
+# make a variable called "test1 scores"
+# set test1 scores to 20% * the users scores for test1
+# make a variable called "test2 scores"
+# set test2 scores to 40% * the users scores for test2
+# make a variable called "test3 scores"
+# set test3 scores to 40% * the users scores for test3
+# print "your final scores are test1 scores +test2 scores+test3 scores"
+
+# test 1 = test 1 * 20%
+# test 2 = test 2 * 40%
+# test 3 = test 3 * 40%
+# student's final score = test 1 + test 2 + test 3
