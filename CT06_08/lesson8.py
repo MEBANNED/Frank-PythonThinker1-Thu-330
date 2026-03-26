aabed67d7d9e7510597f3271421513be3718dca8
MEBANNED <frankchencty@gmail.com>
2026-03-26 11:10:40 +0000

    LESSON 10 yay not happy don't take my YAY as a celebration

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,169 +1,165 @@
-# # Lesson 8 - Importing Libraries, Boolean & Conditions
+"""
+# Lesson 8 - Importing Libraries, Boolean & Conditions
 
+## Recap 1: Product of 5 numbers
 
+Write a program to calculate the product (multiplication) of 5
+numbers.
 
+1. Using a for loop, ask the user for 5 numbers one at a time.
+2. Calculate the multiplication for these 5 numbers and print
+   it out.
+"""
+# total = 1
+# for i in range(1, 6):
+#     number = input("what is number " + str(i) + "?")
+#     total = total * number
+# print("total: " + str(total))
+"""
+---------------------------------------------------------------
 
-# ## Recap 1: Product of 5 numbers
+## Task 1: 'time' library
 
-# Write a program to calculate the product (multiplication) of 5
-# numbers.
-
-# 1. Using a for loop, ask the user for 5 numbers one at a time.
-# 2. Calculate the multiplication for these 5 numbers and print
-#    it out.
-   
-# ---------------------------------------------------------------
-
-# ## Task 1: 'time' library
-
-# **Task 1a**:
-# Import the 'time' library and make use of the 'time.sleep()'
-# function to create a 10 seconds countdown timer that counts
-# to 1, printing the number of seconds remaining every second.
-
-# **Task 1b**:
-# Modify your code from Task 1a to include an 'input()' function
-# asking the user for the number to countdown from, before
-# counting down every second from the number given by the user.
+**Task 1a**:
+Import the 'time' library and make use of the 'time.sleep()'
+function to create a 10 seconds countdown timer that counts
+to 1, printing the number of seconds remaining every second.
+"""
 # import time
-# secs = input("WHAT AMT OF SECS?")
-# for i in range(secs, 0, -1):
+# for i in range(10, 0, -1):
 #     print(i)
 #     time.sleep(1)
-# ---------------------------------------------------------------
+"""
+**Task 1b**:
+Modify your code from Task 1a to include an 'input()' function
+asking the user for the number to countdown from, before
+counting down every second from the number given by the user.
 
-# ## Task 2: 'random' library
+---------------------------------------------------------------
 
-# **Task 2a**:
-# Import the 'random' library and create a program that randomly
-# output a number between 1 to 6
+## Task 2: 'random' library
 
-# **Task 2b**:
-# Using the 'random' library, create 20 numbers between 0 and
-# 9999 randomly.
+**Task 2a**:
+Import the 'random' library and create a program that randomly
+output a number between 1 to 6
+"""
+# import random
+# ran = random.randint(1, 6)
+"""
+**Task 2b**:
+Using the 'random' library, create 20 numbers between 0 and
+9999 randomly.
+"""
 
-# ---------------------------------------------------------------
+"""
+---------------------------------------------------------------
 
-# ## Task 3: Print Boolean Value & Condition
+## Task 3: Print Boolean Value & Condition
 
-# **Task 3a**:
-# Assign a boolean value to a variable and print it.
-a = True
-print(a)
-# **Task 3b**:
-# Create 2 variables both holding the "True" boolean.
-# Print out the result of comparing the 2 variables using
-# the "==" operator.
-b= True
-print(a == b)
-# **Task 3c**:
-# Now, assign 1 variable the "True" boolean, and assign another
-# variable the "False" boolean.
-x = True
-y = False
-# Print out the result of comparing the 2 variables using
-# the "==" operator.
-print(x == y)
+**Task 3a**:
+Assign a boolean value to a variable and print it.
 
-# ---------------------------------------------------------------
+**Task 3b**:
+Create 2 variables both holding the "True" boolean.
+Print out the result of comparing the 2 variables using
+the "==" operator.
 
-# ## Task 4:
+**Task 3c**:
+Now, assign 1 variable the "True" boolean, and assign another
+variable the "False" boolean.
 
-# **Task 4a**: Math Question Generator
-# Using the 'random' library, generate 2 numbers between 1 and 50
-# that the user must add together.
+Print out the result of comparing the 2 variables using
+the "==" operator.
 
-# The output should be one of the following:
-# 1. True (If the answer is correct)
-# 2. False (If the answer is wrong)
+---------------------------------------------------------------
 
-# Example:
-# What is 2 + 5? << 7 >>
-# True
-# # PSEUDOCODE 
-# 1. import random library
-# 2. generate 2 random numbers between 1 and 50
-# 3. ask user to add the 2 numbers together and input their answer
-# 4. check if user's answer is correct by comparing it to the sum of the 2 random numbers
-# 5. print 'True' if user's answer is correct, otherwise print 'False'
-import random
-num1 = random.randint(1, 50)
-num2 = random.randint(1, 50)
-answer = int(input(f"What is {num1} + {num2}? "))
-if answer == num1 + num2:
-    print("True")
-else:
-    print("False")
-# **Task 4b**: Range Guesser
-# Create a program that generates a random number between 1 and
-# 50.
+## Task 4: Random Number Guessing Game
 
-imp
+Create a simple program to guess a random number:​
+- Create a variable called ‘random_num’ and assign a random integerbetween 1 to 10.​
+- Ask the user for an input 'guess'​
 
-# The user should input a range (two numbers: start and end).
+Your program will check if ‘guess’ is equal to 'random_num'.​
 
-# The program checks if the random number falls within the user's
-# range.
+The output should be one of the following:​
+- If the answer is correct – output "Correct!" ​
+- If the answer is wrong – output "Wrong!​
+"""
+# import random
+# random_num = random.randint(1, 10)
+# guess = int(input("Type a number below in between 1 and 10: \n"))
+# if  guess == random_num:
+#    print("Correct!")
+# else:
+#    print('wrong buddy')
+"""
+## Task 5: Math Question Generator
 
-# The output should be one of the following:
-# 1. True (If the answer is correct)
-# 2. False (If the answer is wrong)
+Create a simple program that generate 2 numbers
+between 1 and 50 that the user must add together.​
 
-# ---------------------------------------------------------------
+Ask the user to input the answer.​
 
-# ## Task 5: Random Number Guessing Game
+The output should be one of the following:​
+- If the answer is correct – output "Correct!"​
+- If the answer is wrong – output "Wrong!​
+"""
+# import random
+# rand1 = random.randint(1, 50)
+# rand2 = random.randint(1, 50)
+# ans = rand1 + rand2
+# usr_ans = int(input("what is " + str(rand1 + " + " + str(rand2) + )))
+"""
+## Task 6: Random Multiplication Quiz
 
-# Create a simple program to guess a random number:
-# a. Create a variable called 'guess' and assign a number that
-#    you are guessing
-# b. Create a variable called 'num1' and assign a random integer
-#    between 1 to 10.
+Create a program that generates a certain number of
+random multiplication questions.​
+1. Ask the user to input how many questions should be asked.​
+2. Multiply 2 random numbers between 1 and10 and save the 'answer'.​
+3. Ask the user to input their answer, 'user_answer'.​
+4. Check if 'user_answer' is equal to 'answer'.
 
-# Your program will check if 'guess' is equal to 'num1'.
+The output should be one of the following:​
+- If the answer is correct – output "Correct!" ​
+- If the answer is wrong – output "Wrong!​
+"""
+# amt_of_questions = int(input("how many questions should i ask?"))
+# for i in range(amt_of_questions):
+#    import random
+#    rand1 = random.randint(1, 50)
+#    rand2 = random.randint(1, 50)
+#    ans = rand1 + rand2
+#    usr_ans = int(input("what is " + str(rand1) + " + " + str(rand2) + str(rand1)))
+"""
+## Task 7: Even or Odd Checker
 
-# The output should be one of the following:
-# 1. True (If the answer is correct)
-# 2. False (If the answer is wrong)
+Write a program that asks the user to enter a number. The
+program then tells the user whether the number is even
+(True) or odd (False).
 
-# ---------------------------------------------------------------
+Your program needs to:
+1. Ask user for an integer input.
+2. Check if there is any remainder when user input is divided
+   by 2 (using '%').
+3. Print 'True' if number is even, otherwise print 'False'.
+"""
+# num = int(input("gimme a number"))
+# if num % 2 == 1:
+#    print("odd number")
+# else:
+#    print("even number") 
+"""
+## Task 8: Multiple Check Program
 
-# ## Task 6: Random Multiplication Quiz
+Create a program where the user enters 2 numbers. The
+program then checks if the first number is a multiple of
+the second number.
 
-# You have been tasked by Ms Tan, the Math teacher to create a
-# multiplication quiz.
+Your program needs to:
+1. Get user to input 2 numbers.
+2. Check if there is any remainder when number #1 is divided
+   by number #2
+3. Print 'True' if number #1 is a multiple of number #2,
+   otherwise print 'False'.
+"""
 
-# Create a program that generates a certain number of random
-# multiplication questions.
-
-# Each question should involve multiplying 2 random numbers
-# between 1 and 10. The user should input the number of questions
-# they want to attempt.
-
-# ---------------------------------------------------------------
-
-# ## Task 7: Even or Odd Checker
-
-# Write a program that asks the user to enter a number. The
-# program then tells the user whether the number is even
-# (True) or odd (False).
-
-# Your program needs to:
-# 1. Ask user for an integer input.
-# 2. Check if there is any remainder when user input is divided
-#    by 2 (using '%').
-# 3. Print 'True' if number is even, otherwise print 'False'.
-
-# ---------------------------------------------------------------
-
-# ## Task 8: Multiple Check Program
-
-# Create a program where the user enters 2 numbers. The
-# program then checks if the first number is a multiple of
-# the second number.
-
-# Your program needs to:
-# 1. Get user to input 2 numbers.
-# 2. Check if there is any remainder when number #1 is divided
-#    by number #2
-# 3. Print 'True' if number #1 is a multiple of number #2,
-#    otherwise print 'False'.
--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,79 @@
 print("Hello from lesson 10")
+
+# Task 1: Positive or Negative Numbers
+
+
+
+# import random
+# random_number = random.randint(1,15)
+# print(random_number)
+# guess = int(input("Guess a number you won't know \n"))
+# if guess == random_number:
+#     print("YOU GOT IT RIGHT HOW DID YOU KNOW???!?!!??!?!?!?!?")
+# else:
+#     print("got it wrong just like i told you")
+              
+"""Task 2"""
+# user_input = input("Enter a number: ")
+# try:
+#     number = float(user_input)
+#     if number > 0:
+#         print(f"{number} is positive.")
+#     elif number == 0:
+#         print(f"{number} is neutral.")
+#     else:
+#         print(f"{number} is negative.")
+# except ValueError:
+#     print("Invalid input. Please enter a valid number.")
+
+"""Task 2"""
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("Child")
+# else:
+#     if age >= 13 and age <= 19:
+#         print("Teen")
+#     else:
+#         print("Adult")
+# """Task 3"""
+# temperature = int(input("Enter the temperature in degrees: "))
+# if temperature > 30:
+#     print("Suggest swimming")
+# elif temperature >= 25 and temperature <= 30:
+#     print("Suggest basketball")
+# elif temperature >= 20 and temperature <= 24:
+#     print("Suggest cycling")
+# else:
+#     print("Suggest reading indoors")
+"""Task 4"""
+# Task 4: Grading System (Singapore Primary School AL)
+# score = int(input("Enter your score: "))
+# try:
+#     if score > 90:
+#         print("AL1")
+#     elif score >= 85 and score <= 89:
+#         print("AL2")
+#     elif score >= 80 and score <= 84:
+#         print("AL3")
+#     elif score >= 75 and score <= 79:
+#         print("AL4")
+#     elif score >= 65 and score <= 74:
+#         print("AL5")
+#     elif score >= 45 and score <= 64:
+#         print("AL6")
+#     elif score >= 20 and score <= 44:
+#         print("AL7")
+#     else:
+#         print("AL8")
+# except ValueError:
+#     print("I'm sure that's not your score")
+
+"""Task 5"""
+# age = int(input("Enter your age: "))
+
+# if age < 0:
+#     print("Age cannot be negative")
+# elif age >= 18:
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible to vote")
