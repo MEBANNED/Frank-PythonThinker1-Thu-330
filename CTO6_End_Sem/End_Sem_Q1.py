112f3f2537254b875569f06661f368425d0ef924
MEBANNED <frankchencty@gmail.com>
2026-04-30 10:20:22 +0000

    dccd

---
+++ CTO6_End_Sem/End_Sem_Q1.py
@@ -0,0 +1,21 @@
+"""Task 1"""
+
+counter = 10
+
+while counter < 210:
+    if counter % 10 == 0:
+        print(counter)
+    counter += 1
+
+
+"""Task 2"""
+
+# pas = "superpass123"
+# ans = input("Enter Password: \n")
+
+# if ans != pas:
+#     print("Access Denied")
+# else:
+#     print("Access Granted")
+"""Please uncomment task two before running it"""
+
